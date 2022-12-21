2af46c576b71c18753583781d4e998ed835b699e
randiltennakoon <randil2016al@gmail.com>
2022-12-21 07:47:20 +0530

    used ngFor directive to retrieve  products details

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -7,6 +7,40 @@ import { Component } from '@angular/core';
 })
 export class ProductsComponent {
 
-  public productDate = "2020.11.20";
+  public products = [
+    {
+      'productId' : "001",
+      'productName' : "White Basmathi Rice",
+      'createdDate' : "2020.11.10",
+      'quantity' : 500,
+      'unitPrice' : "400",
+      'productDescription' : "White Basmathi Rice imported from Pakistan"
+    },
+    {
+      'productId' : "002",
+      'productName' : "Dhal",
+      'createdDate' : "2021.10.15",
+      'quantity' : 320,
+      'unitPrice' : "240",
+      'productDescription' : "Sri Lankan organic Dhal"
+    },
+    { 
+      'productId' : "003",
+      'productName' : "sugar",
+      'createdDate': "Jan 29, 2020",
+      'quantity': 1200,
+      'unitPrice': "200",
+      'productDescription':"White sugar manufactured by Palwatte Factory"
+    },
+    { 
+      'productId' : "004",
+      'productName' : "Flour",
+      'createdDate': "Jan 29, 2020",
+      'quantity': 50,
+      'unitPrice': "190",
+      'productDescription':"Super Fine Whole grain general Purpose flour"
+      },
+  ]
+
 
 }
