6329f09475e15abac048226fbeb1a9d6469fbcaf
randiltennakoon <randil2016al@gmail.com>
2023-01-06 13:52:26 +0530

    tested angular lifecycle hooks

--- src/app/components/add-product/add-product.component.ts
+++ src/app/components/add-product/add-product.component.ts
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AddProductComponent {
 
+  constructor() {}
+
+  // Angular Lifecycle Hooks
+  ngOnInit(): void {
+    alert("ngOnInit called");
+    console.log("triggered ngOnInit");
+  }
+
+  // ngOnChanges(): void {
+
+  // }
+
+  ngDoCheck(): void {
+    console.log("triggered ngDoCheck");
+  }
+
+  ngAfterContentInit(): void {
+    console.log("triggered ngAfterContentInit");
+  }
+
+  ngAfterContentChecked(): void {
+    console.log("triggered ngAfterContentChecked");
+  }
+
+  ngAfterViewInit(): void {
+    console.log("triggered ngAfterViewInit");
+  }
+
+  ngAfterViewChecked(): void {
+    console.log("triggered ngAfterViewChecked");
+  }
+
+  ngOnDestroy(): void {
+    alert("ngOnDestroy called");
+    console.log("triggered ngOnDestroy");
+  }
+
 }
